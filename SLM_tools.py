54946d4ea34e9c9007cf987114d7e7074548fd33
OkovTAU16 <omrikovarsky@mail.tau.ac.il>
2024-03-02 12:08:42 +0200

    "creat_time_vec checked,added testing.ipynb to not_used"

--- SLM_tools.py
+++ SLM_tools.py
@@ -6,9 +6,10 @@ from scipy import interpolate
 class SLM_tools:
     @staticmethod
     def creat_time_vec(data):
-        t = np.arange(0, len(data), 1)
+        t = np.arange(0, np.shape(data)[1], 1)
+        # t = np.reshape(t, np.shape(data))
         data_new = interpolate.interp1d(t, data, kind="linear")
-        return data_new
+        return data_new.y, t
 
 
 if __name__ == "__main__":
