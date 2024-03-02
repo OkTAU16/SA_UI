3beaeb9787334c2b0b95bf58ae87474e87e4f1cf
OkovTAU16 <omrikovarsky@mail.tau.ac.il>
2024-03-02 11:06:31 +0200

    "creat_time_vec"

--- SLM_tools.py
+++ SLM_tools.py
@@ -1,9 +1,14 @@
 import Rbeast as rb
+import numpy as np
+from scipy import interpolate
 
 
 class SLM_tools:
-    def __init__(self):
-        a = 1
+    @staticmethod
+    def creat_time_vec(data):
+        t = np.arange(0, len(data), 1)
+        data_new = interpolate.interp1d(t, data, kind="linear")
+        return data_new
 
 
 if __name__ == "__main__":
