e864d124ce819578182d685bde5cfce51a96aa1a
Idan-Frenkel-Weizmann <idan.frenkel@weizmann.ac.il>
2024-05-29 21:06:22 +0300

    SLM-UI V1 - adding all users inputs. solving input problems, fonts and colors

--- SLM_UI.py
+++ SLM_UI.py
@@ -12,6 +12,9 @@ from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.filechooser import FileChooserIconView
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import ScreenManager, Screen
+# from SLM_UI_graphs_window import *
 import os
 
 
@@ -45,10 +48,16 @@ class FileDropApp(App):
         self.drop_area_width = None
         self.drop_area_y = None
         self.drop_area_x = None
+        self.cluster_num = None
+        self.CV_num = None
+        self.target_num = None
 
     def build(self):
         layout = FloatLayout()
-
+        """
+        playing around. not for usage
+        
+        
         self.label_checkbox = Label(text="CheckBox lookout", size_hint=(None, None), pos_hint={'center_x': 0.9, 'center_y': 0.95})
         self.label_radio = Label(text="radio lookout", size_hint=(None, None),
                                     pos_hint={'center_x': 0.9, 'center_y': 0.8})
@@ -69,6 +78,7 @@ class FileDropApp(App):
 
         layout.add_widget(self.label_toggle)
         layout.add_widget(self.toggle_button)
+        """
         '''
         # Create a CheckBox
         self.checkbox = CheckBox(active=False, size_hint=(None, None), size=(50, 50), pos_hint={'right': 0.9, 'top': 0.9})
@@ -83,6 +93,80 @@ class FileDropApp(App):
         layout.add_widget(self.radio1)
         layout.add_widget(self.radio2)
         '''
+        self.label_time_series = Label(text="Added Time Series", size_hint=(None, None),
+                                    pos_hint={'center_x': 0.7, 'center_y': 0.9})
+        #self.label_time_series_Yes = Label(text="Yes", size_hint=(None, None),
+        #                            pos_hint={'center_x': 0.65, 'center_y': 0.85})
+        #self.label_time_series_No = Label(text="No", size_hint=(None, None),
+        #                            pos_hint={'center_x': 0.75, 'center_y': 0.85})
+        layout.add_widget(self.label_time_series)
+        #layout.add_widget(self.label_time_series_Yes)
+        #layout.add_widget(self.label_time_series_No)
+        self.button_time_series_yes = ColorCheckBox(group='time_series', size_hint=(None, None), color=(0, 1, 1, 1), size=(50, 50), pos_hint={'x': 0.63, 'top': 0.87})
+
+        self.button_time_series_no = ColorCheckBox(group='time_series', size_hint=(None, None), size=(50, 50), pos_hint={'x': 0.73, 'top': 0.87})
+        layout.add_widget(self.button_time_series_yes)
+        layout.add_widget(self.button_time_series_no)
+
+        self.label_PCA = Label(text="PCA", size_hint=(None, None),
+                                    pos_hint={'center_x': 0.7, 'center_y': 0.77})
+        layout.add_widget(self.label_PCA)
+
+        self.button_PCA_yes = ColorCheckBox(group='PCA', size_hint=(None, None), color=(0, 1, 1, 1), size=(50, 50), pos_hint={'x': 0.63, 'top': 0.74})
+
+        self.button_PCA_no = ColorCheckBox(group='PCA', size_hint=(None, None), size=(50, 50), pos_hint={'x': 0.73, 'top': 0.74})
+        layout.add_widget(self.button_PCA_yes)
+        layout.add_widget(self.button_PCA_no)
+        self.button_PCA_yes.bind(on_press=self.PCA_show)
+        self.button_PCA_no.bind(on_press=self.PCA_show)
+
+        self.PCA_input = TextInput(hint_text="Enter number of clusters", font_size=16, input_filter='int',multiline=True,
+                                    size_hint=(0.1, 0.06),pos_hint={'x': 0.79, 'top': 0.74})
+        self.button_PCA_submit = Button(text="✓", size_hint=(None, None), font_name='DejaVuSans.ttf', size=(53, 53), pos_hint={'x': 0.9, 'top': 0.74})
+        layout.add_widget(self.PCA_input)
+        layout.add_widget(self.button_PCA_submit)
+        self.PCA_input.opacity = 0
+        self.button_PCA_submit.opacity = 0
+        self.button_PCA_submit.bind(on_press=self.PCA_submit)
+        self.PCA_input.bind(text=self.text_size_change)
+
+
+
+
+        self.label_CV = Label(text="CV", size_hint=(None, None),
+                                    pos_hint={'center_x': 0.7, 'center_y': 0.64})
+        layout.add_widget(self.label_CV)
+
+        self.button_CV_yes = ColorCheckBox(group='CV', size_hint=(None, None), color=(0, 1, 1, 1), size=(50, 50), pos_hint={'x': 0.63, 'top': 0.60})
+
+        self.button_CV_no = ColorCheckBox(group='CV', size_hint=(None, None), size=(50, 50), pos_hint={'x': 0.73, 'top': 0.60})
+        layout.add_widget(self.button_CV_yes)
+        layout.add_widget(self.button_CV_no)
+        self.button_CV_yes.bind(on_press=self.CV_show)
+        self.button_CV_no.bind(on_press=self.CV_show)
+
+        self.CV_input = TextInput(hint_text="Enter number of ???", font_size=16, input_filter='int',multiline=True,
+                                    size_hint=(0.1, 0.06),pos_hint={'x': 0.79, 'top': 0.60})
+        self.button_CV_submit = Button(text="✓", size_hint=(None, None), font_name='DejaVuSans.ttf', size=(53, 53), pos_hint={'x': 0.9, 'top': 0.60})
+        layout.add_widget(self.CV_input)
+        layout.add_widget(self.button_CV_submit)
+        self.CV_input.opacity = 0
+        self.button_CV_submit.opacity = 0
+        self.button_CV_submit.bind(on_press=self.CV_submit)
+        self.CV_input.bind(text=self.text_size_change)
+
+        self.label_targets = Label(text="Number of targets", size_hint=(None, None),
+                                    pos_hint={'center_x': 0.7, 'center_y': 0.5})
+        layout.add_widget(self.label_targets)
+        self.target_input = TextInput(hint_text="Enter number of parameters", font_size=15, input_filter='int',multiline=True,
+                                    size_hint=(0.09, 0.06),pos_hint={'x': 0.63, 'top': 0.46})
+        self.button_targets_submit = Button(text="✓", size_hint=(None, None), font_name='DejaVuSans.ttf', size=(53, 53), pos_hint={'x': 0.73, 'top': 0.46})
+        layout.add_widget(self.target_input)
+        layout.add_widget(self.button_targets_submit)
+        self.target_input.bind(text=self.text_size_change)
+        self.button_targets_submit.bind(on_press=self.target_submit)
+
+
         # Create a label to display dropped file path
         self.file_label = Label(text='Drop files here', size_hint=(None, None),
                                 pos_hint={'center_x': 0.25, 'center_y': 0.9})
@@ -213,6 +297,96 @@ class FileDropApp(App):
         """
 
         pass
+
+    def PCA_show(self, instance):
+        # Show or hide TextInput and V button based on selected radio button
+        if instance == self.button_PCA_yes and instance.state == 'down':
+            self.PCA_input.opacity = 1
+            self.button_PCA_submit.opacity = 1
+        elif instance == self.button_PCA_no and instance.state == 'down':
+            self.PCA_input.opacity = 0
+            self.button_PCA_submit.opacity = 0
+            self.cluster_num = None
+            self.PCA_input.foreground_color = (0, 0, 0, 1)  # RGBA for default color
+            self.PCA_input.background_color = (1, 1, 1, 1)
+            self.PCA_input.text = ''
+            self.PCA_input.font_size = 16
+        else:
+            self.PCA_input.opacity = 0
+            self.button_PCA_submit.opacity = 0
+            self.cluster_num = None
+            self.PCA_input.foreground_color = (0, 0, 0, 1)  # RGBA for default color
+            self.PCA_input.background_color = (1, 1, 1, 1)
+            self.PCA_input.text = ''
+            self.PCA_input.font_size = 16
+
+    def CV_show(self, instance):
+        # Show or hide TextInput and V button based on selected radio button
+        if instance == self.button_CV_yes and instance.state == 'down':
+            self.CV_input.opacity = 1
+            self.button_CV_submit.opacity = 1
+        elif instance == self.button_CV_no and instance.state == 'down':
+            self.CV_input.opacity = 0
+            self.button_CV_submit.opacity = 0
+            self.CV_num = None
+            self.CV_input.foreground_color = (0, 0, 0, 1)  # RGBA for default color
+            self.CV_input.background_color = (1, 1, 1, 1)
+            self.CV_input.text = ''
+            self.CV_input.font_size = 16
+        else:
+            self.CV_input.opacity = 0
+            self.button_CV_submit.opacity = 0
+            self.CV_num = None
+            self.CV_input.foreground_color = (0, 0, 0, 1)  # RGBA for default color
+            self.CV_input.background_color = (1, 1, 1, 1)
+            self.CV_input.text = ''
+            self.CV_input.font_size = 16
+
+    def PCA_submit(self, instance):
+        user_input = self.PCA_input.text
+        if user_input:
+            self.cluster_num = user_input
+            print(self.cluster_num)
+            self.PCA_input.foreground_color = (0, 0.6, 0, 1)  # RGBA for green
+            self.PCA_input.background_color = (0.6, 1, 0.9, 1)
+        else:
+            self.cluster_num = None
+            print(self.cluster_num)
+            self.PCA_input.foreground_color = (0, 0, 0, 1)  # RGBA for default color
+
+    def CV_submit(self, instance):
+        user_input = self.CV_input.text
+        if user_input:
+            self.CV_num = user_input
+            print(self.CV_num)
+            self.CV_input.foreground_color = (0, 0.6, 0, 1)  # RGBA for green
+            self.CV_input.background_color = (0.6, 1, 0.9, 1)
+        else:
+            self.CV_num = None
+            print(self.CV_num)
+            self.CV_input.foreground_color = (0, 0, 0, 1)  # RGBA for default color
+            self.CV_input.background_color = (1, 1, 1, 1)
+
+    def target_submit(self, instance):
+        user_input = self.target_input.text
+        if user_input:
+            self.target_num = user_input
+            print(self.target_num)
+            self.target_input.foreground_color = (0, 0.6, 0, 1)  # RGBA for green
+            self.target_input.background_color = (0.6, 1, 0.9, 1)
+        else:
+            self.target_num = None
+            print(self.target_num)
+            self.target_input.foreground_color = (0, 0, 0, 1)  # RGBA for default color
+            self.target_input.background_color = (1, 1, 1, 1)
+
+    def text_size_change(self, instance, value):
+        print(f"Text changed to: {value}")  # Debug statement
+        if value:
+            instance.font_size = 28  # Larger font size for user input
+
+        else:
+            instance.font_size = 8  # same size as before
 
 if __name__ == '__main__':
     FileDropApp().run()
